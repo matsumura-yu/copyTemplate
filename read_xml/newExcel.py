6ec61adc68769ba4cd53df1db466edcaa1a03bb8
matsumura-yu <matsumura.yu@isid.co.jp>
2018-09-05 12:29:06 +0900

    Add feature xml -> excelList

--- read_xml/newExcel.py
+++ read_xml/newExcel.py
@@ -32,6 +32,9 @@ def dictionaryToListExcel():
     # 一番左上
     sheet['A1'] = '脆弱性/シナリオ'
 
+    # 脆弱性のリストを作成
+    vulsList = []
+
     # 脆弱性の種類と件数を埋める
     for index, scenario in enumerate(scenarioList):
         print(str(index + 1) + "番目")
@@ -42,13 +45,28 @@ def dictionaryToListExcel():
 
         # 脆弱性名挿入
         # 重複したとき用に脆弱性のリストを作成
-        vulsList = []
+        # ここにあると二週目に初期化されてしまう
+        # vulsList = []
 
         for vuls_index, vuls in enumerate(scenario["vuls"]):
-            vulsList.append(vuls["vulName"])
-            sheet.cell(row = vuls_index + 2, column = 1).value = vuls["vulName"]
-            sheet.cell(row = vuls_index + 2, column = 2).value = vuls["count"]
-            sheet.cell(row = vuls_index + 2, column = 2).data_type = 'n'
+
+            # 既出の脆弱性を排除
+            if vuls["vulName"] in vulsList:
+                # リストの何番目に格納されているか, 0始まり
+                # 上書きする
+                vuls_index = vulsList.index(vuls["vulName"])
+            else:
+                # 新規の脆弱性
+                vulsList.append(vuls["vulName"])
+                # 何番目の脆弱性か検出
+                vuls_index = vulsList.index(vuls["vulName"])
+                # 新しく項目を足す
+                sheet.cell(row = vuls_index + 2, column = 1).value = vuls["vulName"]
+                print(vuls["vulName"]+"を追加しました")
+            
+            sheet.cell(row = vuls_index + 2, column = index + 2).value = vuls["count"]
+            # 数値データとして挿入
+            sheet.cell(row = vuls_index + 2, column = index + 2).data_type = 'n'
         print(vulsList)
     wb.save('list.xlsx')
 
